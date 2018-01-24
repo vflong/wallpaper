d8c33dedc52df2dfdb036170e008cf0bbf34163a
vflong <weifeilong2013@gmail.com>
2018-01-24 23:17:28 +0800

    use for loop :wink

--- path.py
+++ path.py
@@ -8,21 +8,13 @@ config = configparser.ConfigParser()
 config_file = os.path.join(os.path.dirname(__file__), 'config.ini')
 config.read(config_file)
 
-wallpaper_path = os.path.join(user_home, config['Paths']['wallpaper_path'])
-src_path = os.path.join(user_home, config['Paths']['src_path'])
-bing_path = os.path.join(user_home, config['Paths']['bing_path'])
-bingcom_path = os.path.join(user_home, config['Paths']['bingcom_path'])
-spotlight_path = os.path.join(user_home, config['Paths']['spotlight_path'])
-pc_path = os.path.join(user_home, config['Paths']['pc_path'])
-tablet_path = os.path.join(user_home, config['Paths']['tablet_path'])
-theme_path = os.path.join(user_home, config['Paths']['theme_path'])
-
 
 def mkdirs():
-    path_list = [wallpaper_path, src_path, bing_path, bingcom_path, spotlight_path, pc_path, tablet_path, theme_path]
-    for image_path in path_list:
+    path_name = ['wallpaper_path', 'src_path', 'bing_path', 'bingcom_path', 'spotlight_path', 'pc_path', 'tablet_path', 'theme_path']
+    path_list = []
+    for image_path in path_name:
+        image_path = os.path.join(user_home, config['Paths'][image_path])
         os.makedirs(image_path, exist_ok=True)
+        path_list.append(image_path)
     return path_list
-
-
 
