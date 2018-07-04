efbc6216ff399466fc454288573999dbd7cbc461
vflong <weifeilong2013@gmail.com>
2018-07-04 21:57:37 +0800

    add test

---
+++ wallpaper/test.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+Get Bing Picture.
+"""
+
+from utils import get_bingcom_src_file
+
+
+def main():
+    status = get_bingcom_src_file()
+
+
+if __name__ == "__main__":
+    main()
